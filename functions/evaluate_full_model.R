21f23c62d7941635cc6dffc231961f0bbd9ea20e
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2022-03-24 11:29:31 +0100

    still fixing hierarchical evaluation

--- functions/evaluate_full_model.R
+++ functions/evaluate_full_model.R
@@ -34,9 +34,6 @@ predict_with_PlastoGram <- function(ngram_models, hmm_models, higher_level_model
     df
   }) 
   ngram_models_res <- reduce(ngram_preds_list, full_join, by = "seq_name")
-  if(grepl("Filtering_0.5", architecture_name)) {
-    ngram_models_res <- filter_predictions_according_to_hierarchy(ngram_models_res, architecture_df)
-  }
   
   tat_res <- predict_profileHMM(test_sequences, hmm_models[["Tat_model"]], remove_files = FALSE) 
   sec_res <- predict_profileHMM(test_sequences, hmm_models[["Sec_model"]], remove_files = FALSE) 
@@ -52,22 +49,28 @@ predict_with_PlastoGram <- function(ngram_models, hmm_models, higher_level_model
   hmm_models_res <- full_join(sec_res, tat_res, by = "seq_name")
   
   all_res <- left_join(ngram_models_res, hmm_models_res, by = "seq_name")
-  all_res[is.na(all_res)] <- 0
+  
+  filtered_all_res <- if(grepl("Filtering_0.5", architecture_name)) {
+    filter_predictions_according_to_hierarchy(all_res, architecture_df)
+  } else {
+    all_res
+  }
+  filtered_all_res[is.na(filtered_all_res)] <- 0
   
   glm_preds <- if(class(higher_level_model) == "ranger") {
     data.frame(seq_name = test_df[["seq_name"]],
                dataset = test_df[["dataset"]],
-               predict(higher_level_model, all_res)[["predictions"]])  %>% 
+               predict(higher_level_model, filtered_all_res)[["predictions"]])  %>% 
       mutate(Localization = colnames(.)[3:ncol(.)][max.col(.[, colnames(.)[3:ncol(.)]])])
     
   } else {
     data.frame(seq_name = test_df[["seq_name"]],
                dataset = test_df[["dataset"]],
-               predict(higher_level_model, all_res, type = "probs"),
-               Localization = predict(higher_level_model, all_res))
+               predict(higher_level_model, filtered_all_res, type = "probs"),
+               Localization = predict(higher_level_model, filtered_all_res))
   }
   
-  list("Lower-order_models_preds" = all_res,
+  list("Lower-order_models_preds" = filtered_all_res,
        "Higher-order_model_preds" = glm_preds,
        "Final_results" = data.frame(glm_preds[, c("seq_name", "dataset", "Localization")],
                                     Probability = sapply(1:nrow(glm_preds[, 3:(ncol(glm_preds)-1)]), function(i) max(glm_preds[i, 3:(ncol(glm_preds)-1)]))))
